aa8fc0eeeeeeeff291e763df3407291d9aa22a70
Delanyo Nutakor <dnutak26@colby.edu>
2024-12-18 10:58:31 -0500

    Refactored app/main contents to meet pylint standards

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,9 @@
+"""
+This module initializes the main blueprint for the Flask application.
+"""
+
 from flask import Blueprint
 
 main = Blueprint('main', __name__, template_folder='templates')
 
-from . import routes  # Import routes to register with the blueprint
+from . import routes  # pylint: disable=wrong-import-position
