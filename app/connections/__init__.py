a3b4df9d7fa00f41c610383babed7684c4023a9f
Delanyo Nutakor <dnutak26@colby.edu>
2024-12-18 20:51:19 -0500

    Refactor app/connections content to meet pylint standards

--- app/connections/__init__.py
+++ app/connections/__init__.py
@@ -1,5 +1,9 @@
+"""
+This module initializes the Blueprint for the 'connections' feature of the Flask application.
+"""
+
 from flask import Blueprint
 
 connections = Blueprint('connections', __name__, template_folder='templates')
 
-from . import routes  # Import routes to register with the blueprint
+from . import routes  # pylint: disable=wrong-import-position
